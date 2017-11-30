08766d3b4fde240464a84db4643aab5fe7ff39a5
Chase Paules <cpaules@gmail.com>
2017-11-29 19:33:29 -0500

    updated missing import

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import rootReducer from './reducers'
 
 const store=createStore(rootReducer, applyMiddleware(thunk))
 
