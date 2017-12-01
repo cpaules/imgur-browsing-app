5d8d1611125c9ec71c21a34129a2b83ef2d5a1ef
Chase Paules <cpaules@gmail.com>
2017-11-30 19:55:50 -0500

    got fetch request semi working

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,33 +2,56 @@ import React, { Component } from 'react';
 import './App.css';
 import Images from './Images'
 
-const images = [
-  {
-    id: "asdj",
-    title: "title1",
-    link: "https://i.imgur.com/uOOY2zZ.jpg"
-  },
-  {
-    id: "asda",
-    title: "title2",
-    link: "https://i.imgur.com/4uuNGG4.jpg"
-  },
-  {
-    id: "fasf",
-    title: "title3",
-    link: "https://i.imgur.com/tlkRooa.jpg"
-  }
-]
+// const images = [
+//   {
+//     id: "asdj",
+//     title: "title1",
+//     link: "https://i.imgur.com/uOOY2zZ.jpg"
+//   },
+//   {
+//     id: "asda",
+//     title: "title2",
+//     link: "https://i.imgur.com/4uuNGG4.jpg"
+//   },
+//   {
+//     id: "fasf",
+//     title: "title3",
+//     link: "https://i.imgur.com/tlkRooa.jpg"
+//   }
+// ]
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.imgur.com/3/gallery/hot/viral/day/0', { 
+      headers: { 'Authorization': 'Client-ID 93482c2dc9b13f9' }
+    })
+    .then(function(response) {
+      response.json()
+      .then(function(data) {
+        console.log(data)
+      })
+    })
+      //.then(imgur => this.setState( { data }))
+  }
+
   render() {
     return (
         <div className="App">
           app
-          <Images images={images} />
+          <Images images={this.state.images} />
         </div>
     );
   }
+
 }
 
 export default App;
