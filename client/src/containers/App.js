0306cff189745a0375d6c5decec8113a24191120
Chase Paules <cpaules@gmail.com>
2017-12-03 00:26:23 -0500

    added navbar with routes/links to home/favorites/about

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom'
 import './App.css';
 import Images from './Images'
+//import NavBar from '../components/NavBar'
 
 class App extends Component {
 
   render() {
     return (
+      <Router>      
         <div className="App">
-          app
-          <Images />
+          <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
+            <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
+            <NavLink style={{ marginRight: '10px' }} to="/favorites">Favorites</NavLink>
+            <NavLink style={{ marginRight: '10px' }} to="/about">About</NavLink> 
+          </div>
+          <Route exact path="/" component={Images} />
         </div>
+      </Router>
     );
   }
 
