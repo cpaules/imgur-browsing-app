5e0960cb361582598dbf1bbc8d4ee5efe73bb7f2
Chase Paules <cpaules@gmail.com>
2017-12-03 13:00:13 -0500

    added imageshow.js and added links to show page on each image card

--- client/src/components/ImageCard.js
+++ client/src/components/ImageCard.js
@@ -3,8 +3,8 @@ import React from 'react';
 const ImagesCard = ({ image }) => (
     <div key={image.id} className="ImageCard">
       {image.is_album ? 
-      <img className="ImgurImage" src={image.images[0].link} alt={image.title} /> 
-      : <img className="ImgurImage" src={image.link} alt={image.title} /> }
+      <a href={image.id}><img className="ImgurImage" src={image.images[0].link} alt={image.title} /></a> 
+      : <a href={image.id}><img className="ImgurImage" src={image.link} alt={image.title} /></a> }
       <p className="ImgurImageTitle"> {image.title} </p>
     </div>
   )
