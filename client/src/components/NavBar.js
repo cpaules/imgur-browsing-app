cdff061914a6045d6780ae3510e2f4e983400350
Chase Paules <cpaules@gmail.com>
2017-12-04 18:54:38 -0500

    modified navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,24 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NavBar = props => {
   return (
     <div className="App-nav-link" style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/hot"
-      >
-        Hot
-      </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/top"
-      >
-        Top
-      </NavLink>
+      <NavLink style={{ marginRight: '10px' }} to="/">Home</NavLink>
+      <NavLink style={{ marginRight: '10px' }} to="/favorites">Favorites</NavLink>
+      <NavLink style={{ marginRight: '10px' }} to="/about">About</NavLink>
     </div>
   );
 }
